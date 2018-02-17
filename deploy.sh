b6f7b2ec6860a618d34c8e3f714d5bce889ac276
whimzee <priestchees@gmail.com>
2018-02-17 17:06:20 +0100

    deploy improvements

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# deploy.sh gets run on every push to master
+# this file gets run on every push to master
 
+# make sure our dependencies are correct
+npm prune
 npm install
+
+# build project
 ng build --output-path /var/www/html
-# ng build --output-path /var/www/html --prod
