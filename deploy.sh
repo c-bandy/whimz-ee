f533a14e24f6bff331fab0490ac771d208f5ac28
whimzee <priestchees@gmail.com>
2018-02-18 18:01:09 +0100

    correct npm order

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@
 ## this file gets run on every push to master
 
 # make sure our dependencies are correct
-npm update
 npm install
+npm update
 
 # build project
 ng build --output-path /var/www/html
