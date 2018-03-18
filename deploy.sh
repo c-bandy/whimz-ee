7872f533caa8cbcaa45104560257557c7b30bab1
Benjamin Åberg <ben@whimz.ee>
2018-03-18 15:26:03 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,11 @@
 ## deploy.sh
 # this file gets run on every push to master
 
-# make sure our dependencies are up to date
+# install our dependencies
 npm install
 
 # build project
 npm run build:universal
+
+# reload node deamon
+pm2 reload www.whimz.ee
