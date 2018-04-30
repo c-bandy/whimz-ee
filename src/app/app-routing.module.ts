4b1ff4e2cd6c1681ef942c1fc4dcccb84e7dff4a
Benjamin Åberg <ben@whimz.ee>
2018-04-30 20:26:13 +0200

    update name and status

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     canActivate: [MetaGuard],
     data: {
       meta: {
-        title: 'ben aberg',
+        title: 'cassidy',
         override: true,
         description: 'web & ui design for the modern world'
       }
